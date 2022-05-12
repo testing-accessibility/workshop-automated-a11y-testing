787d8f33ba4e428339c2d672303d6358446d840e
Marcy Sutton <holla@marcysutton.com>
2022-05-12 13:44:04 -0700

    Improve accessibility of icon button and test

--- components/icon-button.js
+++ components/icon-button.js
@@ -2,13 +2,14 @@ import React from "react"
 
 const IconButton = ({name, onClick}) => {
     return (
-        <div
+        <button
+            aria-label={name}
             className="btn-submit btn-lookingglass"
             data-testid="btn-submit"
             onClick={(event) => { onClick(event)}}
         >
             <span className="icon-lookingglass-white"></span>
-        </div>
+        </button>
     )
 }
 
