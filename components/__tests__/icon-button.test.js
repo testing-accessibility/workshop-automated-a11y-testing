6147c66d628b28a46ee98f78b136c0efe6b86bfb
Marcy Sutton <holla@marcysutton.com>
2022-05-11 19:34:28 -0700

    Fix failing test

--- components/__tests__/icon-button.test.js
+++ components/__tests__/icon-button.test.js
@@ -14,7 +14,7 @@ const user = userEvent.setup()
 describe('IconButton', () =>{
     it('labels the button', () => {
         const textFixture = "Send it!"
-        const { getByLabelText } = render(<IconButton buttonName={textFixture} />)
+        const { getByLabelText } = render(<IconButton name={textFixture} />)
 
         const buttonText = getByLabelText(textFixture)
 
