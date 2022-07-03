766ae9b9b342ffdae21c8d6803b3ce20ec228124
Taylor Bell <taylorbell@gmail.com>
2022-07-03 11:57:33 -0600

    restore failing icon-button test

--- components/__tests__/icon-button.test.js
+++ components/__tests__/icon-button.test.js
@@ -2,15 +2,22 @@
  * @jest-environment jsdom
  */
 
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import '@testing-library/jest-dom'
-import userEvent from '@testing-library/user-event'
-
-import IconButton from "../icon-button"
-
-const user = userEvent.setup()
-
-describe('IconButton', () => {
-
-})
+ import React from 'react'
+ import { render, screen, fireEvent, getByText, getByLabelText } from '@testing-library/react'
+ import '@testing-library/jest-dom'
+ import userEvent from '@testing-library/user-event'
+ 
+ import IconButton from "../icon-button"
+ 
+ const user = userEvent.setup()
+ 
+ describe('IconButton', () =>{
+     it('labels the button', () => {
+         const textFixture = "Send it!"
+         const { getByLabelText } = render(<IconButton buttonName={textFixture} />)
+ 
+         const buttonText = getByLabelText(textFixture)
+ 
+         expect(buttonText).toBeInTheDocument()
+     })
+ })
