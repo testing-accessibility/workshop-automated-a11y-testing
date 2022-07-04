d4b7c70735b1ca0ae7d136de3cef16cf05deef14
Taylor Bell <taylorbell@gmail.com>
2022-07-04 14:30:40 -0600

    add header test to homepage tests

--- exercise3-cypress-integration/HomePage.test.js
+++ exercise3-cypress-integration/HomePage.test.js
@@ -2,4 +2,8 @@ describe('HomePage', () => {
     beforeEach(() => {
         cy.visit('http://localhost:1234/')
     })
+
+    it('should have a Header', () => {
+        cy.get('#header').contains('CampSpots')
+    })
 })
